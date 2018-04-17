bc68aca64767fa34a1690ffed92254b13629aadd
llenk <ellenkeal@gmail.com>
2018-04-17 11:36:19 -0500

    create listening server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 npm means "node package manager"
 # to do this 
 - npm init
+- put in all the info
+- npm install --save express
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000; // all caps bc it'll never change
+// the port is which entryway on our computer we're going to use
+// there are thousands, so just pick one
+// if you try one and get red text pick a different one
+// there are like 9000 of them
+
+app.listen(PORT); 
